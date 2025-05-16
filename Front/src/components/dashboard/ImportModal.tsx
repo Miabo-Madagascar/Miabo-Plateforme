a28308ce01bd74bd7613b6fafccd9b87f151c1ac
tsilavina007 <andriamiart114@gmail.com>
2025-05-16 15:41:52 +0300

    🐞 fix: onClose and max width for modals #2

--- Front/src/components/dashboard/ImportModal.tsx
+++ Front/src/components/dashboard/ImportModal.tsx
@@ -21,7 +21,7 @@ const ImportModal: React.FC<ImportModalProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed top-[-35px]  inset-0 z-50 flex items-center justify-center bg-black/50 " onClick={onClose}>
+    <div className="fixed top-[-35px]  inset-0 z-50 flex items-center justify-center bg-black/50 ">
       <div className="bg-white p-6 rounded-lg shadow-xl w-full max-w-lg mx-4 animate-fade-in">
         <h2 className="text-2xl font-bold mb-4">Importer un fichier</h2>
         <form onSubmit={handleImport} className="space-y-4">
--- Front/src/components/dashboard/newDocumentModal.tsx
+++ Front/src/components/dashboard/newDocumentModal.tsx
@@ -18,7 +18,7 @@ export default function NewDocumentModal({ isOpen, onClose }: NewDocumentModalPr
   if (!isOpen) return null;
 
   return (
-    <div className="fixed top-[-35px] inset-0 z-50 flex items-center justify-center bg-black/50" onClick={onClose}>
+    <div className="fixed top-[-35px] inset-0 z-50 flex items-center justify-center bg-black/50">
       <div className="bg-white p-6 rounded-lg shadow-xl w-full max-w-lg mx-4 animate-fade-in">
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
